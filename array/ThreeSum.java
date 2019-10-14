da0a2ee060737a72f71f1270a9f69f5aa3c9ccad
xiaoxiong <itzhuxiaoxiong@163.com>
2019-10-14 10:15:27 +0800

    three sum solution

--- array/ThreeSum.java
+++ array/ThreeSum.java
@@ -22,26 +22,25 @@ public class ThreeSum {
      */
     public List<List<Integer>> threeSum(int[] nums) {
 
-//        if (nums == null || nums.length < 3) {
-//            throw new IllegalArgumentException("数组长度不能小于3");
-//        }
 
         List<List<Integer>> ans = new ArrayList<>();
 
         if (nums == null || nums.length < 3) {
             return ans;
         }
+
+        //排序
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length - 1; i++) {
+        for (int i = 0; i < nums.length; i++) {
 
             //当前数>0时  后面的均不符合条件
             if (nums[i] > 0) {
                 break;
             }
 
-            //todo
-            if (i > 1 && nums[i] == nums[i - 1]) {
+            //因为数组前面是排序过的 所以当前值如果与左边下一个数一样 会导致相同的结果 故++
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
@@ -51,11 +50,21 @@ public class ThreeSum {
 
             while (l < r) {
                 int sum = nums[i] + nums[l] + nums[r];
+                //得保证任何条件下都对l或者r有操作
                 if (sum == 0) {
+                    //如果找到符合条件的值 加入结果集
                     ans.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    //并判断确认双指针的动作
 
-                    //todo
-
+                    //因为数组前面是排序过的 所以当前值如果与左边下一个数一样 会导致相同的结果 故++
+                    while (l < r && nums[l] == nums[l + 1]) {
+                        l++;
+                    }
+                    while (l < r && nums[r] == nums[r - 1]) {
+                        r--;
+                    }
+                    l++;
+                    r--;
                 } else if (sum > 0) {
                     r--;
                 } else {
@@ -73,11 +82,17 @@ public class ThreeSum {
     @Test
     public void testThreeSum() {
 
-        int[] nums = {-1,0,1,2,-1,-4};
+        int[] nums = {-1, 0, 1, 2, -1, -4};
 
-        List<List<Integer>> lists = threeSum(nums);
+        List<List<Integer>> ans = threeSum(nums);
 
-        Assert.assertEquals(2,lists.size());
-        Assert.assertEquals("[[-1,-1,2],[-1,0,1]]",lists);
+        Assert.assertEquals(2, ans.size());
+
+
+        List<List<Integer>> expectedList = new ArrayList<>();
+        expectedList.add(Arrays.asList(-1, -1, 2));
+        expectedList.add(Arrays.asList(-1, 0, 1));
+
+        Assert.assertEquals(expectedList, ans);
     }
 }
