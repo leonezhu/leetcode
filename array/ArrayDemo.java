143d99a13b4bc1a4790c4dee3248581848345feb
xiaoxiong <itzhuxiaoxiong@163.com>
2019-11-06 11:39:46 +0800

    无重复数组求最长字符滑动窗口优化解

---
+++ array/ArrayDemo.java
@@ -0,0 +1,17 @@
+package array;
+
+/**
+ * @author XiaoXiong
+ * @since 2019-11-05
+ */
+public class ArrayDemo {
+
+
+    public static void main(String[] args) {
+        int oldCapacity = 10;
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+
+        System.out.println("new size =" + newCapacity);
+
+    }
+}
--- array/LongestSubStringSolution.java
+++ array/LongestSubStringSolution.java
@@ -3,12 +3,9 @@ package array;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
-/**
+/*
  * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
  * <p>
@@ -78,45 +75,69 @@ public class LongestSubStringSolution {
                 set.add(s.charAt(j++));
                 ans = Math.max(ans, j - i);
             } else {
-                set.remove(s.charAt(i++));
+                set.remove(s.charAt(i++)); // 需要从头往后遍历
             }
         }
         return ans;
     }
 
-    //TODO 优化method2
+    //
 
-
-    /**  参考答案的暴力写法
-     *     public static int getLongestSubString(String originStr) {
-     *         int max = 0;
-     *         List<Character> container = new ArrayList<>();
-     *         for (int i = 0; i < originStr.length(); i++) {
-     *             for (int j = i + 1; j < originStr.length(); j++) {
-     *                 if (allUnique(originStr, i, j)) {
-     *                     max = Math.max(max, j - i);
-     *                 }
-     *             }
-     *         }
-     *         return max;
-     *     }
+    /**
+     * 滑动窗口优化
+     * test string : abccdce
+     * 当j=4时，current char = c ，map中存在，此时i需要跳转至j+1即d处开始
      *
-     *     public static boolean allUnique(String s, int start, int end) {
-     *         Set<Character> set = new HashSet<>();
-     *         char[] chars = s.toCharArray();
-     *         for (int i = start; i < end; i++) {
-     *             if (set.contains(chars[i])) {
-     *                 return false;
-     *             }
-     *             set.add(chars[i]);
-     *         }
-     *         return true;
-     *     }
+     * @param s
+     * @return
      */
+    public static int method3(String s) {
+        int n = s.length(), ans = 0;
+        //key=不重复的字符  value=当key重复时i需要跳转的index 即j+1
+        Map<Character, Integer> map = new HashMap<>(n);
+        for (int j = 0, i = 0; j < n; j++) {
+
+            if (map.containsKey(s.charAt(j))) {
+                int index = map.get(s.charAt(j));
+                i = Math.max(index, i); //直接跳到不重复那个字符所在的index
+
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
 
 
+        return ans;
+    }
 
 
+    /**
+     * 参考答案的暴力写法
+     * public static int getLongestSubString(String originStr) {
+     * int max = 0;
+     * List<Character> container = new ArrayList<>();
+     * for (int i = 0; i < originStr.length(); i++) {
+     * for (int j = i + 1; j < originStr.length(); j++) {
+     * if (allUnique(originStr, i, j)) {
+     * max = Math.max(max, j - i);
+     * }
+     * }
+     * }
+     * return max;
+     * }
+     * <p>
+     * public static boolean allUnique(String s, int start, int end) {
+     * Set<Character> set = new HashSet<>();
+     * char[] chars = s.toCharArray();
+     * for (int i = start; i < end; i++) {
+     * if (set.contains(chars[i])) {
+     * return false;
+     * }
+     * set.add(chars[i]);
+     * }
+     * return true;
+     * }
+     */
 
 
     private String testStr = "abccdce";
@@ -144,5 +165,16 @@ public class LongestSubStringSolution {
 
 
     }
-}
 
+    @Test
+    public void testMethod3() {
+
+        System.out.println("testStr=" + testStr);
+        Assert.assertEquals(3, LongestSubStringSolution.method3(testStr));
+
+        System.out.println("testStr=" + testStr2);
+        Assert.assertEquals(6, LongestSubStringSolution.method3(testStr2));
+
+
+    }
+}
